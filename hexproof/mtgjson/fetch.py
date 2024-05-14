50d9a9304196ecedb10b00b068e4a80676107834
Chris <hexproofsystems@gmail.com>
2024-05-14 15:51:28 -0500

    feat(mtgjson): Implement full schema spec from MTGJSON docs
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- hexproof/mtgjson/fetch.py
+++ hexproof/mtgjson/fetch.py
@@ -9,12 +9,12 @@ from pathlib import Path
 import requests
 from ratelimit import sleep_and_retry, RateLimitDecorator
 from backoff import on_exception, expo
-from omnitils.fetch import request_header_default
+from omnitils.fetch import request_header_default, download_file
 from omnitils.files.archive import unpack_tar_gz
 
 # Local Imports
 from hexproof.mtgjson.enums import MTGJsonURL
-from hexproof.mtgjson.schema import types as MTGJsonTypes
+from hexproof.mtgjson import schema as MTGJsonTypes
 
 # Rate limiter to safely limit MTGJSON requests
 mtgjson_rate_limit = RateLimitDecorator(calls=20, period=1)
@@ -57,27 +57,28 @@ def request_handler_mtgjson_gql(func) -> Callable:
 
 
 """
-* Request Funcs
+* Requesting JSON Assets
 """
 
 
 @request_handler_mtgjson
-def get_meta() -> MTGJsonTypes.Meta:
-    """MTG.Meta: Get the current metadata resource for MTGJSON."""
+def get_data_meta() -> MTGJsonTypes.Meta:
+    """Get the current MTGJSON 'Meta' resource."""
     with requests.get(
         url=MTGJsonURL.API_META,
         headers=request_header_default.copy()
     ) as res:
         res.raise_for_status()
-        return res.json().get('data', {})
+        meta_obj = res.json().get('data', {})
+        return MTGJsonTypes.Meta(**meta_obj)
 
 
 @request_handler_mtgjson
-def get_set(card_set: str) -> MTGJsonTypes.Set:
-    """Grab available set data from MTG Json.
+def get_data_set(card_set: str) -> MTGJsonTypes.Set:
+    """Get a target MTGJSON 'Set' resource.
 
     Args:
-        card_set: The set to look for, ex: MH2
+        card_set: The set to look for, e.g. MH2
 
     Returns:
         MTGJson set dict or empty dict.
@@ -87,23 +88,70 @@ def get_set(card_set: str) -> MTGJsonTypes.Set:
         headers=request_header_default.copy()
     ) as res:
         res.raise_for_status()
-        return res.json().get('data', {})
+        set_obj = res.json().get('data', {})
+        return MTGJsonTypes.Set(**set_obj)
 
 
 @request_handler_mtgjson
-def get_set_list() -> list[MTGJsonTypes.SetList]:
-    """Grab the current 'SetList' MTGJSON file."""
+def get_data_set_list() -> list[MTGJsonTypes.SetList]:
+    """Get the current MTGJSON 'SetList' resource."""
     with requests.get(
         url=MTGJsonURL.API_SET_LIST,
         headers=request_header_default.copy().copy()
     ) as res:
         res.raise_for_status()
-        return res.json().get('data', [])
+        set_list = res.json().get('data', [])
+        return [MTGJsonTypes.SetList(**n) for n in set_list]
+
+
+"""
+* Downloading JSON Assets
+"""
+
+
+@request_handler_mtgjson
+def get_meta(path: Path) -> Path:
+    """Stream a target MTGJSON 'Meta' resource and save it to a file.
+
+    Args:
+        path: Path object where the JSON data will be saved.
+    """
+    download_file(
+        url=MTGJsonURL.API_META,
+        path=path)
+    return path
+
+
+@request_handler_mtgjson
+def get_set(card_set: str, path: Path) -> Path:
+    """Stream a target MTGJSON 'Set' resource and save it to a file.
+
+    Args:
+        card_set: The set to look for, ex: MH2
+        path: Path object where the JSON data will be saved.
+    """
+    download_file(
+        url=(MTGJsonURL.API / card_set.upper()).with_suffix('.json'),
+        path=path)
+    return path
+
+
+@request_handler_mtgjson
+def get_set_list(path: Path) -> Path:
+    """Stream the current MTGJSON 'SetList' resource and save it to a file.
+
+    Args:
+        path: Path object where the JSON data will be saved.
+    """
+    download_file(
+        url=MTGJsonURL.API_SET_LIST,
+        path=path)
+    return path
 
 
 @request_handler_mtgjson
 def get_sets_all(path: Path) -> Path:
-    """Download the current JSON file archive from the 'AllSetFiles' MTGJSON endpoint.
+    """Stream the current MTGJSON 'AllSetFiles' archive, save it, and extract it.
 
     Args:
         path: Directory to unpack the 'AllSetFiles' MTGJSON archive.
@@ -111,13 +159,11 @@ def get_sets_all(path: Path) -> Path:
     Returns:
         Path to the unpacked 'AllSetFiles' MTGJSON directory.
     """
-    path = path / 'AllSetFiles.tar.gz'
-    res = requests.get(
+    archive = path / 'AllSetFiles.tar.gz'
+    download_file(
         url=MTGJsonURL.API_SET_ALL,
-        headers=request_header_default.copy().copy())
-    with open(path, 'wb') as f:
-        f.write(res.content)
+        path=archive)
 
     # Unpack the contents
-    unpack_tar_gz(path)
-    return Path(path.parent) / 'AllSetFiles'
+    unpack_tar_gz(archive)
+    return Path(path / 'AllSetFiles')
