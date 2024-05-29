c50da86317828079f499aa6deac929742fc3d247
Chris <hexproofsystems@gmail.com>
2024-05-29 14:02:11 -0500

    refactor(vectors/fetch): Separate caching and request funcs, remove deprecated `update_vectors_manifest`
    Signed-off-by: Chris <hexproofsystems@gmail.com>

--- hexproof/vectors/fetch.py
+++ hexproof/vectors/fetch.py
@@ -2,20 +2,20 @@
 * MTG Vectors Request Handling
 """
 # Standard Library Imports
-from contextlib import suppress
 from pathlib import Path
 from typing import Optional, Union
 
 # Third Party Imports
 from loguru import logger
 from omnitils.api.github import gh_get_data_json, gh_download_file
-from omnitils.files import mkdir_full_perms, dump_data_file
+from omnitils.files import mkdir_full_perms
 from omnitils.files.archive import unpack_zip
 from requests import RequestException
 import yarl
 
 # Local Imports
 from hexproof.vectors.enums import VectorURL
+from hexproof.vectors import schema as VectorSchema
 
 """
 * Request Funcs
@@ -23,11 +23,45 @@ from hexproof.vectors.enums import VectorURL
 
 
 def get_vectors_manifest(
+    url: Union[yarl.URL, str, None] = None,
+    header: Optional[dict] = None,
+    auth_token: Optional[str] = None
+) -> Optional[VectorSchema.Manifest]:
+    """Gets the current mtg-vectors symbol manifest and returns it as a 'Manifest' object.
+
+    Args:
+        url: URL to fetch manifest from, uses official `mtg-vectors` repository if not provided.
+        header: Header object to pass with request, uses default if not provided.
+        auth_token: Optional auth token to pass with request, increases rate limits.
+
+    Returns:
+        A mtg-vectors 'Manifest' object.
+    """
+    try:
+        data = gh_get_data_json(
+            url=url or VectorURL.Manifest,
+            header=header,
+            auth_token=auth_token)
+        return VectorSchema.Manifest(**data)
+    except RequestException:
+        return logger.error('Unable to reach mtg-vectors manifest URL!')
+    except FileExistsError:
+        return logger.error('Unable to write new mtg-vectors manifest!')
+    except (OSError, ValueError):
+        return logger.error('Unable to dump data file!')
+
+
+"""
+* Download Funcs
+"""
+
+
+def cache_vectors_manifest(
     path: Path,
     url: Union[yarl.URL, str, None] = None,
     header: Optional[dict] = None,
     auth_token: Optional[str] = None
-) -> Optional[dict]:
+) -> Optional[Path]:
     """Gets the current mtg-vectors symbol manifest.
 
     Args:
@@ -40,12 +74,12 @@ def get_vectors_manifest(
         Path to the manifest file.
     """
     try:
-        data = gh_get_data_json(
+        _path = gh_download_file(
             url=url or VectorURL.Manifest,
+            path=path,
             header=header,
             auth_token=auth_token)
-        dump_data_file(data, path)
-        return data
+        return _path
     except RequestException:
         return logger.error('Unable to reach mtg-vectors manifest URL!')
     except FileExistsError:
@@ -54,7 +88,7 @@ def get_vectors_manifest(
         return logger.error('Unable to dump data file!')
 
 
-def get_vectors_package(
+def cache_vectors_package(
         directory: Path,
         url: Union[yarl.URL, str, None] = None,
         header: Optional[dict] = None,
@@ -98,41 +132,3 @@ def get_vectors_package(
     except Exception as e:
         logger.exception(e)
         logger.error('Unable to unpack vectors package!')
-
-
-"""
-* Update Funcs
-"""
-
-
-def update_vectors_manifest(
-    path: Path,
-    current: str,
-    url: Union[yarl.URL, str, None] = None,
-    force: bool = False
-) -> Optional[dict]:
-    """Update the mtg-vectors manifest.
-
-    Args:
-        path: Path to save the manifest file.
-        current: Current version of vector manifest.
-        url: URL to fetch manifest from.
-        force: Whether to update even if metadata is already up-to-date, False by default.
-
-    Returns:
-        Metadata if successful, otherwise None.
-    """
-
-    # Request the updated manifest
-    manifest = get_vectors_manifest(path=path, url=url)
-    if not manifest:
-        return
-
-    # Check if manifest version changed
-    with suppress(Exception):
-        if current == manifest.get('meta', {}).get('version', '') and not force:
-            return logger.info('Set symbols already up-to-date!')
-        return manifest
-
-    # General error occurred
-    return logger.error("MTG Vectors updated set symbol manifest couldn't be written!")
